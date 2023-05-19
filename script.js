c0e29f31c955837be1f2032c9db7686ec2fad851
Kalleby Evangelho <82461649+KallebyX@users.noreply.github.com>
2023-05-18 21:56:48 -0300

    Update script.js

--- script.js
+++ script.js
@@ -45,14 +45,10 @@ function renderElements(taskList) {
   htmlList.innerHTML = "";
 
   // Ajustar a lógica
-  let card = createCard(taskList[0]);
-  htmlList.appendChild(card);
-
-  card = createCard(taskList[1]);
-  htmlList.appendChild(card);
-
-  card = createCard(taskList[2]);
-  htmlList.appendChild(card);
+  for (let i = 0; i < taskList.length; i++) {
+    let card = createCard(taskList[i]);
+    htmlList.appendChild(card);
+  }
 }
 
 renderElements(tasks);
